a8ce434c9f8aaffb449ea64a0de0a57183dd5e26
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2022-12-24 23:39:48 +0630

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import "../styles/themes/prism-laserwave.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
