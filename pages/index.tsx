83273d23a725181150b4f0bb63d67989660337ba
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2022-12-24 12:30:49 +0630

    run npx @next/codemod next-image-to-legacy-image .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { Fragment } from "react";
 
 import { Popover, Transition } from "@headlessui/react";
