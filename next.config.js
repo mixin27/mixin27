9ef8a0ce5a3b73ee8e80f34ae58886ee0f440e17
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2022-12-24 12:26:22 +0630

    create next app and home page layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      "tailwindui.com",
+      "images.unsplash.com",
+      "eincode.com",
+      "thrangra.sirv.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
