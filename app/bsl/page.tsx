0318060cb0a460e504403bcaef7dba550f69a1b4
mixin27 <kyawzayartun.dev@gmail.com>
2024-11-24 14:11:22 +0630

    update assetlinks.json for app links

---
+++ app/bsl/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div>Billion Sport Live app link page.</div>;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/google8f06b5535b3e9018.html",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/google8f06b5535b3e9018.html",
+      },
+    ];
+  },
 };
 
 export default withContentlayer(nextConfig);
